43ab949b0db3c4c1d3f67da676b88017f67b2504
Carlos_Santaella <carlos.santaella@orbit.es>
2023-06-19 08:55:39 +0200

    Docker-compose para terraform

---
+++ deploy/main.tf
@@ -0,0 +1,14 @@
+terraform {
+    backend "s3" {
+        bucket = "csantaella-proyecto-devops-tfstate"
+        key = "app.tfstate"
+        region = "eu-west-1"
+        encrypt = true
+        dynamodb_table = "csantaella-db-proyecto-devops-tfstate"
+  }
+}
+
+provider "aws" {
+    region = "eu-west-1"
+    version = "= 4.5.0"
+}
