b9dd48a434f983524677802fd7efbfd71b124ab6
Carlos_Santaella <carlos.santaella@orbit.es>
2023-06-27 08:28:03 +0200

    Incluimos Terraform Init-Validate-Fmt y quitamos DB de Outputs

--- deploy/outputs.tf
+++ deploy/outputs.tf
@@ -1,8 +1,8 @@
 # Salidas de ciertas variables o atributos de uno de los recursos
 
-output "db_host" {
-  value = aws_db_instance.main.address
-}
+# output "db_host" {
+#   value = aws_db_instance.main.address
+# }
 
 output "bastion_host" {
   value = aws_instance.bastion.public_dns
