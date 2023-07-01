5ed9f53ac0d1dde20a826450f25a80aafbc1154f
Carlos_Santaella <carlos.santaella@orbit.es>
2023-07-01 17:59:06 +0200

    Corrección problemas DNS-workspace

--- deploy/ecs.tf
+++ deploy/ecs.tf
@@ -128,7 +128,7 @@ resource "aws_ecs_service" "api" {
   load_balancer {
         target_group_arn = aws_lb_target_group.api.arn
         container_name   = "proxy"
-        container_port   = 8000
+        container_port   = 80
     }
 }
 
